e476445f2990353dded6c9d93bc0f430fccf9f07
Sverrir Pall Sverrisson <sverrirps@gmail.com>
2016-04-04 10:13:42 +0000

    maoi adjustments.

--- app/scripts/game.js
+++ app/scripts/game.js
@@ -93,8 +93,8 @@ window.Game = (function() {
 	/**
 	 * Some shared constants.
 	 */
-	Game.prototype.WORLD_WIDTH = 100;
-	Game.prototype.WORLD_HEIGHT = 60;
+	Game.prototype.WORLD_WIDTH = 102.4;
+	Game.prototype.WORLD_HEIGHT = 57.6;
 
 	return Game;
 })();
